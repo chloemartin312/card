e8125dc522ecb41c81330dae119f223426d10f39
chloemartin312 <chloemartin312@outlook.com>
2025-09-24 16:08:41 -0400

    fancified a little

--- src/my-card.js
+++ src/my-card.js
@@ -1,11 +1,5 @@
 import { LitElement, html, css } from 'lit';
 
-/**
- * Now it's your turn. Here's what we need to try and do:
- * 1. Get you HTML from your card working in here 
- * 2. Get your CSS rescoped as needed to work here
- */
-
 export class MyCard extends LitElement {
 
   static get tag() {
@@ -17,6 +11,7 @@ export class MyCard extends LitElement {
     this.cardTitle = "Allium";
     this.image = "https://assets.highcountrygardens.com/media/catalog/product/a/l/allium-globemaster-1_1.jpg?quality=85&fit=bounds&height=&width=3840&auto=webp&format=pjpg";
     this.description = "This is an allium flower.";
+    this.fancy = false;
   }
 
   static get styles() {
@@ -26,14 +21,18 @@ export class MyCard extends LitElement {
       }
 
       :host([fancy]) .card{
-        background-color: var(--my-card-fancy-bg )
+        background-color: pink;
+        border: 2px solid hotpink;
+        box-shadow: 10px 5px 5px #9569a3;
       }
-      
+
+      :host([fancy]) .text {
+          color: indigo;
+        }
+
       .card {
         max-width: 400px;
-        box-sizing: border-box;
         padding: 16px;
-        margin: 16px;
         text-align: center;
         border: 5px solid pink;
         border-radius: 8px;
@@ -50,10 +49,20 @@ export class MyCard extends LitElement {
       }
 
       button:hover {
-        background-color: pink;
+        background-color: hotpink;
         color: black;
       }
     `;
+  }
+
+  openChanged(e) {
+    console.log(e.newState);
+    if (e.newState === "open") {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    }
   }
 
   render() {
@@ -62,16 +71,23 @@ export class MyCard extends LitElement {
             <h3 class="text">${this.cardTitle}</h3>
             <img class="card-image" src="${this.image}" alt="Picture of ${this.cardTitle}">
             <p class="text">${this.description}</p>
-            <a href="https://hax.psu.edu"> 
-              <button>Details</button>
-            </a>
+            <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+              <summary>Description</summary>
+              <div>
+                <slot></slot>
+                <a href="https://hax.psu.edu" target="_blank"> 
+                  <button class = "btn">Details</button>
+                </a>
+              </div>
+            </details>
         </div>
     `;
   }
 
+
+
   static get properties() {
     return {
-      title: {type: String },
       cardTitle: {type: String},
       image: {type: String },
       description: { type: String},
